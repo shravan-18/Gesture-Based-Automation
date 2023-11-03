427625ba0ea6600ef01933d251e0b8317d4fa174
Shravan Venkatraman <145443739+shravan-18@users.noreply.github.com>
2023-11-03 08:12:48 +0530

    Added runner file

---
+++ OpenCV/tempCodeRunnerFile.py
@@ -0,0 +1,13 @@
+pygame.mixer.init()
+        # # Specify the path to your song file
+        # songs = os.listdir(song_dir)
+        # random_song = random.choice(songs)
+        # song_file = os.path.join(song_dir, random_song)
+
+        # # Load the song
+        # pygame.mixer.music.load(song_file)
+        # # Play the song
+        # pygame.mixer.music.play()
+
+        # while pygame.mixer.music.get_busy():
+        #     continue
